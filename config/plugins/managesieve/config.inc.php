cef71f7dc47c3b644caa96699e902925c7c10fba
Mark Hahl <mark@insipid.ai>
2020-03-29 11:37:04 +1100

    add files

--- config/plugins/managesieve/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $config['managesieve_port'] = 4190;
-  $config['managesieve_debug'] = true;
-  $config['managesieve_host'] = 'tls://mail.emesis.io';
-  $config['managesieve_usetls'] = true;
-
